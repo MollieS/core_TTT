e17db484deef27690ec862bc665d57bca38f8455
Mollie Stephenson <molliestephenson@Mollies-MacBook-Pro.local>
2016-05-16 16:45:24 +0100

    runs in terminal

--- src/test/java/ttt/HumanPlayerTest.java
+++ src/test/java/ttt/HumanPlayerTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class HumanPlayerTest {
 
     @Test
-    public void hasASymbol() {
+    public void hasAMark() {
         Player player = new HumanPlayer("X");
         assertEquals("X", player.getMark());
     }
