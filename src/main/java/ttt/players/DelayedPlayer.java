de5b62e90a48acfc523a5c3802256f0b1c7b81b2
Mollie Stephenson <mollie@8thlight.com>
2016-07-05 10:56:28 +0100

    Changes delayed player tests

--- src/main/java/ttt/players/DelayedPlayer.java
+++ src/main/java/ttt/players/DelayedPlayer.java
@@ -7,9 +7,16 @@ import ttt.game.Marks;
 public class DelayedPlayer implements Player {
 
     private Player player;
+    private Integer delay;
 
-    public DelayedPlayer(Player player) {
+    public DelayedPlayer(RandomPlayer player) {
         this.player = player;
+        this.delay = 1000;
+    }
+
+    public DelayedPlayer(PerfectPlayer player) {
+        this.player = player;
+        this.delay = 0;
     }
 
     public Marks getMark() {
@@ -17,20 +24,26 @@ public class DelayedPlayer implements Player {
     }
 
     public Integer getLocation(Board board) throws Exception {
-        long startTime = System.currentTimeMillis();
         Integer location = player.getLocation(board);
-        long endTime = System.currentTimeMillis();
-        long duration = (endTime - startTime);
-        return delayResponse(location, duration);
-    }
-
-    public Integer delayResponse(Integer location, long duration) throws InterruptedException {
-        if (duration > 500) {
+        if (board.availableMoves().size() > 9) {
             return location;
         } else {
-            Thread.sleep(1000);
-            return location;
+            this.delay = 1000;
+            return delayResponse(location);
         }
+    }
+
+    public Integer delayResponse(Integer location) {
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            assert false;
+        }
+        return location;
+    }
+
+    public Integer getDelay() {
+        return delay;
     }
 
     public Class<? extends Player> playerType() {
--- src/test/java/ttt/DelayedPlayerTest.java
+++ src/test/java/ttt/DelayedPlayerTest.java
@@ -1,6 +1,5 @@
 package ttt;
 
-import org.junit.Before;
 import org.junit.Test;
 import ttt.game.Board;
 import ttt.game.Marks;
@@ -14,33 +13,22 @@ import static org.junit.Assert.assertTrue;
 public class DelayedPlayerTest {
 
     @Test
-    public void onlyDelaysIfDurationShorterThanHalfASecond() throws InterruptedException {
+    public void delaysRandomPlayerByASecond() throws InterruptedException {
         DelayedPlayer player = new DelayedPlayer(new RandomPlayer(new RandomLocationGenerator(), Marks.X));
-        long start = System.currentTimeMillis();
-        player.delayResponse(2, 600);
-        long end = System.currentTimeMillis();
-        assertTrue(end - start < 1000);
-
+        assertTrue(player.getDelay() == 1000);
     }
 
     @Test
-    public void delaysRandomPlayer() throws Exception {
-        Board board = new Board(3);
-        DelayedPlayer player = new DelayedPlayer(new RandomPlayer(new RandomLocationGenerator(), Marks.X));
-        long start = System.currentTimeMillis();
-        player.getLocation(board);
-        long end = System.currentTimeMillis();
-        assertTrue((end - start) > 1000);
-    }
-
-    @Test
-    public void doesNotDelayPerfectPlayerIfItTakesLongerThanASecond() throws Exception {
-        Board board = new Board(4);
+    public void doesNotDelayPerfectPlayerOnEmptyBoard() throws Exception {
         DelayedPlayer player = new DelayedPlayer(new PerfectPlayer(Marks.X));
-        long start = System.currentTimeMillis();
+        assertTrue(player.getDelay() == 0);
+    }
+
+    @Test
+    public void delaysPerfectPlayerOnSmallerBoard() throws Exception {
+        Board board = new Board(3);
+        DelayedPlayer player = new DelayedPlayer(new PerfectPlayer(Marks.X));
         player.getLocation(board);
-        long end = System.currentTimeMillis();
-        assertTrue((end - start) > 500);
-        assertTrue((end - start) < 2000);
+        assertTrue(player.getDelay() == 1000);
     }
 }
